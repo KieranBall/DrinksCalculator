ef1f354316b21a42165d48f30e6505a505329d96
kieran.ball@my.jcu.edu.au <kieran.ball@my.jcu.edu.au>
2018-03-15 22:43:07 +1000

    added settings

---
+++ app/src/main/java/com/example/kieran/drinkscalculator/Settings.java
@@ -0,0 +1,13 @@
+package com.example.kieran.drinkscalculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Settings extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
