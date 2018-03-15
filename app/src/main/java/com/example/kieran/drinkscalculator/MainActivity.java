97aa788ddf5bc92ffc080a241cd741635b80138a
kieran.ball@my.jcu.edu.au <kieran.ball@my.jcu.edu.au>
2018-03-16 00:37:18 +1000

    add purpose to plus and minus buttons

--- app/src/main/java/com/example/kieran/drinkscalculator/MainActivity.java
+++ app/src/main/java/com/example/kieran/drinkscalculator/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.kieran.drinkscalculator;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +17,34 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String testString = "Nausea\nVomiting\nSevere motor impairment\nLoss of consciousness\nMemory blackout";
+        TextView test = (TextView) findViewById(R.id.testText);
+
+        test.setText(testString);
+
     }
+
+    public void onSettingsClick(View view){
+        Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
+    }
+
+    public void  onPlusClick(View view){
+        EditText plusButton = (EditText) findViewById(R.id.numDrinks);
+        String editTextString = plusButton.getText().toString();
+        BigDecimal drinksNumber = new BigDecimal(editTextString);
+        BigDecimal newDrinksNumber = drinksNumber.add(new BigDecimal("0.1"));
+        plusButton.setText(newDrinksNumber.toString());
+    }
+
+    public void  onMinusClick(View view){
+        EditText plusButton = (EditText) findViewById(R.id.numDrinks);
+        String editTextString = plusButton.getText().toString();
+        BigDecimal drinksNumber = new BigDecimal(editTextString);
+        BigDecimal newDrinksNumber = drinksNumber.subtract(new BigDecimal("0.1"));
+        plusButton.setText(newDrinksNumber.toString());
+    }
+
+
 }
--- app/src/main/java/com/example/kieran/drinkscalculator/Settings.java
+++ app/src/main/java/com/example/kieran/drinkscalculator/Settings.java
@@ -1,7 +1,9 @@
 package com.example.kieran.drinkscalculator;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Settings extends Activity {
 
@@ -9,5 +11,10 @@ public class Settings extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    public void onSaveClick(View view){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
