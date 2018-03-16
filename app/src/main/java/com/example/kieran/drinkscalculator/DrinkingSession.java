a9dfbaa5f97e087a7b5e7ea4812977ff16d1e1ed
kieran.ball@my.jcu.edu.au <kieran.ball@my.jcu.edu.au>
2018-03-16 20:01:28 +1000

    updates

--- app/src/main/java/com/example/kieran/drinkscalculator/DrinkingSession.java
+++ app/src/main/java/com/example/kieran/drinkscalculator/DrinkingSession.java
@@ -8,9 +8,7 @@ public class DrinkingSession {
     public BigDecimal bac = new BigDecimal("0.0");
     public BigDecimal totalStandardDrinks = new BigDecimal("0.0");
     public int numDrinks = 0;
-
-    // number of drinks
-    // standard drinks consumed
+    public BigDecimal alcoholConsumedGrams;
 
 
     public DrinkingSession(int userWeight, boolean isUserMale){
@@ -22,28 +20,25 @@ public class DrinkingSession {
         this.weight = weight;
     }
 
-
     public void setMale(boolean male) {
         isMale = male;
-    }
-
-    public void setBac(BigDecimal bac) {
-        this.bac = bac;
     }
 
     public BigDecimal getBac() {
         return bac;
     }
 
-    public void calculateBac(){
-
-
-    }
-
     public void addDrink(String standardDrink){
         numDrinks++;
         totalStandardDrinks = totalStandardDrinks.add(new BigDecimal(standardDrink));
         calculateBac();
+    }
+
+    public void calculateBac(){
+        alcoholConsumedGrams = totalStandardDrinks.multiply(new BigDecimal("10"));
+
+
+
     }
 
 }
