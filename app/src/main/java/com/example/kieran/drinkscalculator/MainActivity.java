8e73d57553a9c5b581ab45aa4bc415a57e070cab
kieran.ball@my.jcu.edu.au <kieran.ball@my.jcu.edu.au>
2018-03-18 22:41:19 +1000

    updating theme

--- app/src/main/java/com/example/kieran/drinkscalculator/MainActivity.java
+++ app/src/main/java/com/example/kieran/drinkscalculator/MainActivity.java
@@ -96,7 +96,7 @@ public class MainActivity extends Activity {
     public void onRefreshClick(View view){
         session.calculateBac();
         TextView bacDisplay = (TextView) findViewById(R.id.bac_display);
-        String formatedBacDisplay = session.bac.substring(0, 5);
+        String formatedBacDisplay = session.bac.substring(1, 5);
         bacDisplay.setText(formatedBacDisplay);
         setEffectsText();
 
